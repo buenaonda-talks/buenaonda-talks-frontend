4eb925a1479cda5c8f22d07498f8f2770bb0ee05
Ignacio Guzmán <ignacio658mg@gmail.com>
2024-02-12 17:38:49 -0300

    feat: add sentry on production and remove "images" config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 
-const { protocol, hostname, port, pathname } = new URL(
-    process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
-);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                // protocol: protocol.slice(0, -1),
-                hostname,
-                // port,
-                // pathname: `${pathname}/**`,
-            },
-            {
-                protocol: 'https',
-                hostname: 'secure.gravatar.com',
-            },
-        ],
-    },
     rewrites: async () => [
         {
             source: '/graphql',
@@ -36,43 +18,45 @@ const nextConfig = {
     ],
 };
 
-module.exports = nextConfig;
+if (process.env.NEXT_PUBLIC_APP_ENV === 'production') {
+    // Injected content via Sentry wizard below
+    module.exports = withSentryConfig(
+        nextConfig,
+        {
+            // For all available options, see:
+            // https://github.com/getsentry/sentry-webpack-plugin#options
 
-// Injected content via Sentry wizard below
-// module.exports = withSentryConfig(
-//     nextConfig,
-//     {
-//         // For all available options, see:
-//         // https://github.com/getsentry/sentry-webpack-plugin#options
+            // Suppresses source map uploading logs during build
+            silent: true,
+            org: 'ignacio-guzman',
+            project: 'javascript-nextjs',
+        },
+        {
+            // For all available options, see:
+            // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-//         // Suppresses source map uploading logs during build
-//         silent: true,
-//         org: 'ignacio-guzman',
-//         project: 'javascript-nextjs',
-//     },
-//     {
-//         // For all available options, see:
-//         // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+            // Upload a larger set of source maps for prettier stack traces (increases build time)
+            widenClientFileUpload: true,
 
-//         // Upload a larger set of source maps for prettier stack traces (increases build time)
-//         widenClientFileUpload: true,
+            // Transpiles SDK to be compatible with IE11 (increases bundle size)
+            transpileClientSDK: true,
 
-//         // Transpiles SDK to be compatible with IE11 (increases bundle size)
-//         transpileClientSDK: true,
+            // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+            tunnelRoute: '/monitoring',
 
-//         // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-//         tunnelRoute: '/monitoring',
+            // Hides source maps from generated client bundles
+            hideSourceMaps: true,
 
-//         // Hides source maps from generated client bundles
-//         hideSourceMaps: true,
+            // Automatically tree-shake Sentry logger statements to reduce bundle size
+            disableLogger: true,
 
-//         // Automatically tree-shake Sentry logger statements to reduce bundle size
-//         disableLogger: true,
-
-//         // Enables automatic instrumentation of Vercel Cron Monitors.
-//         // See the following for more information:
-//         // https://docs.sentry.io/product/crons/
-//         // https://vercel.com/docs/cron-jobs
-//         automaticVercelMonitors: true,
-//     },
-// );
+            // Enables automatic instrumentation of Vercel Cron Monitors.
+            // See the following for more information:
+            // https://docs.sentry.io/product/crons/
+            // https://vercel.com/docs/cron-jobs
+            automaticVercelMonitors: true,
+        },
+    );
+} else {
+    module.exports = nextConfig;
+}

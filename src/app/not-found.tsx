ef2fd5ea1009242107ee05c5662eeede20ee4f35
Ignacio Guzmán <ignacio658mg@gmail.com>
2024-03-11 18:30:36 -0300

    feat: better 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,32 @@
+import { Button } from '@/components/ui/button';
+import LandingHeader from '@/screens/landing/header';
+import LandingFooter from '@/screens/landing/landing-footer';
 import Link from 'next/link';
 
 export const dynamic = 'force-dynamic';
 
 export default function NotFound() {
     return (
-        <div>
-            <h2>Not Found</h2>
-            <p>Could not find requested resource</p>
-            <Link href="/">Return Home</Link>
-        </div>
+        <main className="flex min-h-screen flex-col">
+            <LandingHeader />
+
+            <div className="container flex flex-1 items-center justify-center pb-32 pt-36 text-center">
+                <div className="space-y-4">
+                    <div className="space-y-1">
+                        <h1 className="text-4xl font-bold">Parece que te has perdido</h1>
+
+                        <p className="text-muted-foreground">
+                            La página que buscas no existe o ha sido movida.
+                        </p>
+                    </div>
+
+                    <Button asChild className="rounded-full">
+                        <Link href="/">Volver al inicio</Link>
+                    </Button>
+                </div>
+            </div>
+
+            <LandingFooter />
+        </main>
     );
 }

171fb455343ac7d90842348eadcea6c560d552c2
Ignacio Guzmán <ignacio658mg@gmail.com>
2024-03-14 18:16:56 -0300

    type fetchConfig

--- src/api/fetch-client.ts
+++ src/api/fetch-client.ts
@@ -21,7 +21,7 @@ export async function fetchClient<T, V>(
     }
 
     try {
-        const fetchConfig = {
+        const fetchConfig: RequestInit = {
             method: 'POST',
             headers: {
                 Accept: 'application/json',

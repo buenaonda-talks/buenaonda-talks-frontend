528d4f285713340d84eb15663e4a7818c2e22587
Ignacio Guzmán <ignacio658mg@gmail.com>
2024-03-15 14:59:40 -0300

    feat: error screen on not user found

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,11 +6,16 @@ import { StudentsLayout } from '@/screens/dashboard/student/shared/layout';
 import { TeacherLayout } from '@/screens/dashboard/teacher/shared/layout';
 import { DashboardCreateProfile } from './create-profile';
 import { TeacherStudentsTableContainer } from '@/screens/dashboard/teacher/dashboard/table';
+import { DashboardErrorScreen } from '@/screens/dashboard/shared/dashboard-error-screen';
 
 const Page = async () => {
     const user = await fetchUserServer();
 
-    if (user?.user?.isAdmin) {
+    if (!user || !user.user) {
+        return <DashboardErrorScreen />;
+    }
+
+    if (user.user.isAdmin) {
         return (
             <AdminLayout>
                 <DashboardAdminsPageContent />
@@ -18,7 +23,7 @@ const Page = async () => {
         );
     }
 
-    if (user?.user?.isStudent) {
+    if (user.user.isStudent) {
         return (
             <StudentsLayout>
                 <DashboardStudentsHome />
@@ -26,7 +31,7 @@ const Page = async () => {
         );
     }
 
-    if (user?.user?.isTeacher) {
+    if (user.user.isTeacher) {
         return (
             <TeacherLayout requiresVerification={true} user={user.user}>
                 <TeacherStudentsTableContainer />
